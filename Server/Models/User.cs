fe5967f36fc32446a9890335725cb2986d9916f5
DianaElena99 <elenapirvu99@gmail.com>
2021-07-31 18:17:08 +0300

    add all

---
+++ Server/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public String Email { get; set; }
+        public User(String Username_, String Password_, String Email_)
+        {
+            Username = Username_;
+            Password = Password_;
+            Email = Email_;
+        }
+    }
+}
---
+++ Server/Repository/IQueryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Repository
+{
+    interface IQueryRepository<IEntity>
+    {
+        IEntity Get(int Id);
+        IList<IEntity> GetAll();
+        
+    }
+}
