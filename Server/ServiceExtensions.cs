0f519f5de57a455b8e4444d7f5ace441e3dfe617
DianaElena99 <elenapirvu99@gmail.com>
2021-08-13 09:18:04 +0300

    Migrations

---
+++ Server/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureIdentity(this IServiceCollection services) 
+        {
+            var builder = services.AddIdentityCore<User>(u => u.User.RequireUniqueEmail = true);
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
+            builder.AddEntityFrameworkStores<DatabaseContext>().AddDefaultTokenProviders();
+        }
+    }
+}
